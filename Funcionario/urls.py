38d85089aa611e235cd0d5396ab0da76f362e831
Pvilla14 <pablo.villagran.hermanns@gmail.com>
2025-07-07 22:28:30 -0400

    pagina principal Funcionario

---
+++ Funcionario/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', ver_links, name='links'),
+    path('asignaturas_docente/', ver_asignaturas_docente, name='asignaturas_docente'),
+    path('asignaturas_curso/', ver_asignaturas_y_docente_curso, name='asignaturas_curso'),
+    path('crear_alumno/', añadir_alumno, name='añadir_alumno'),
+]
